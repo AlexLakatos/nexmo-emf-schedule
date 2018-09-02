bd8fa900e0c05a1f2197c550ae44e5423afcd3bb
Julia Biro <biro.julia.izabella@gmail.com>
2018-09-02 18:28:05 +0100

    Header alignment with home button
    Aligned elements in header, added home button

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
         <span class="mdl-layout-title"><a href="/">EMF Camp Schedule</a></span>
         <div class="mdl-layout-spacer"></div>
         <nav class="mdl-navigation">
+          <a class="mdl-navigation__link mdl-layout--large-screen-only" href="/">Home</a>
           <a href="https://developer.nexmo.com" target="_blank" class="love">made with 💙 by <br> <img src="https://dashboard.nexmo.com/resources/img/nexmo-logo-w-vonage.svg" /></a>
         </nav>
       </div>
@@ -26,6 +27,7 @@ a.love {
   margin-bottom: 0;
   line-height: 18px;
   margin-left: 14px;
+  font-family: "Roboto","Helvetica","Arial",sans-serif;
 }
 
 .love img {
@@ -38,6 +40,16 @@ a.love {
   }
 }
 
+@media (min-width: 480px) {
+  .mdl-layout-title {
+    font-size: 32px;
+    margin-top: 6px;
+  }
+  .mdl-navigation__link {
+    font-size: 18px;
+    font-family: "Roboto","Helvetica","Arial",sans-serif;
+  }
+}
 
 #app {
   -webkit-font-smoothing: antialiased;
@@ -45,6 +57,9 @@ a.love {
   color: #2c3e50;
 }
 
+.mdl-navigation__link{
+  margin-top: 20px;
+}
 
 .mdl-layout__header-row {
   padding: 0 16px;
